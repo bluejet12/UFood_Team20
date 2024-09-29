e32514f999cd7354cc9d5676e57622349af390af
nathan67003 <nathan.nadeau.1@ulaval.ca>
2024-09-29 11:28:13 -0400

    Carte terminée, interprétation directions à afficher

--- vue-app/src/components/ResturantPage.vue
+++ vue-app/src/components/ResturantPage.vue
@@ -13,7 +13,8 @@
           <strong>{{ day }}:</strong> {{ hours }}
         </li>
       </ul>
-      <p><strong>Location:</strong> Latitude {{ restaurant.location.coordinates[1] }}, Longitude {{ restaurant.location.coordinates[0] }}</p>
+      <p><strong>Location:</strong> Latitude {{ restaurant.location.coordinates[1] }},
+        Longitude {{ restaurant.location.coordinates[0] }}</p>
       <div v-if="restaurant.pictures && restaurant.pictures.length">
         <h3>Pictures</h3>
         <ul>
@@ -23,7 +24,8 @@
         </ul>
       </div>
       <div style="height: 300px">
-        <l-map ref="map" v-model:zoom="zoom" :center="[restaurant.location.coordinates[1], restaurant.location.coordinates[0]]">
+        <l-map ref="map" v-model:zoom="zoom" :center="[restaurant.location.coordinates[1],
+            restaurant.location.coordinates[0]]">
           <l-tile-layer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               layer-type="base"
@@ -77,19 +79,88 @@ export default {
       function success(position){
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
-        url = "http://routing.openstreetmap.de/routed-car/route/v1/driving/";
-        console.log(url);
-        url += longitude + "," + latitude + ";" + restaurant.location.coordinates[0] + ","
-            + restaurant.location.coordinates[1] + "?overview=false&steps=true";
-        console.log(url);
+        url = "http://routing.openstreetmap.de/routed-car/route/v1/driving/" + longitude + "," + latitude + ";"
+            + restaurant.location.coordinates[0] + "," + restaurant.location.coordinates[1]
+            + "?overview=false&steps=true";
         fetch(url).then(function(response) {
           return response.json();
         }).then(function (data) {
-          console.log(data);
           if(data.code === "Ok"){
             let routeData = data.routes[0];
-            console.log(routeData);
-            this.route = "Distance: ";
+            //this.route = "Distance: " + (Math.round(routeData.distance/100)/10) + " km<br><br>";
+            console.log("Distance: " + (Math.round(routeData.distance/100)/10) + " km");
+            //this.route += "DIRECTIONS:<br>";
+            for (let i in routeData.legs[0].steps) {
+              let step = routeData.legs[0].steps[i];
+              console.log(step);
+              let distance;
+              let name = "";
+              let ref1 = "";
+
+              if(step.distance > 1000){
+                distance = (Math.round(step.distance/100)/10) + " km";
+              }
+              else distance = Math.round(step.distance) + " m";
+
+              if(typeof step.ref == "string" && step.ref.search(";") === -1){
+                ref1 = step.ref;
+              }
+              else if(typeof step.ref == "string" && step.ref.search(";") !== -1){
+                ref1 = step.ref.substring(0, step.ref.search(";"));
+              }
+
+              if(step.name !== "" && ref1 !== ""){
+                name = " onto " + step.name + "(" + ref1 + ")";
+              }
+              else if(step.name !== "" && ref1 === ""){
+                name = " onto " + step.name;
+              }
+              else if(step.name === "" && ref1 !== ""){
+                name = " onto " + ref1;
+              }
+
+              if(step.maneuver.type === "depart"){
+                //this.route += "Depart " + step.maneuver.modifier.toUpperCase() + name + " " + distance
+                // + " away<br><br>";
+                console.log("Depart " + step.maneuver.modifier.toUpperCase() + name + " " + distance + " away");
+              }
+              else if(step.maneuver.type === "turn" || step.maneuver.type === "end of road"){
+                //this.route += "Continue " + distance + " then turn " + step.maneuver.modifier.toUpperCase() + name
+                // + "<br><br>";
+                console.log("Continue " + distance + " then turn " + step.maneuver.modifier.toUpperCase() + name);
+              }
+              else if(step.maneuver.type === "new name"){
+                //this.route += "Continue " + distance + " then keep going " + step.maneuver.modifier.toUpperCase()
+                // + name + "<br><br>";
+                console.log("Continue " + distance + " then keep going " + step.maneuver.modifier.toUpperCase() + name);
+              }
+              else if(step.maneuver.type === "on ramp" || step.maneuver.type === "off ramp"){
+                //this.route += "Continue " + distance + " then take the " + step.maneuver.type + " "
+                // + step.maneuver.modifier.toUpperCase() + name + "<br><br>";
+                console.log("Continue " + distance + " then take the " + step.maneuver.type + " "
+                    + step.maneuver.modifier.toUpperCase() + name);
+              }
+              else if(step.maneuver.type === "merge"){
+                //this.route += "Continue " + distance + " then merge " + step.maneuver.modifier.toUpperCase() + name
+                // + "<br><br>";
+                console.log("Continue " + distance + " then merge " + step.maneuver.modifier.toUpperCase() + name);
+              }
+              else if(step.maneuver.type === "fork"){
+                //this.route += "Continue " + distance + " then go " + step.maneuver.modifier.toUpperCase()
+                // + " through the fork" + name + "<br><br>";
+                console.log("Continue " + distance + " then go " + step.maneuver.modifier.toUpperCase()
+                    + " through the fork" + name);
+              }
+              else if(step.maneuver.type === "arrive"){
+                let tempstr = "You have arrived at your destination";
+                console.log(typeof step.maneuver.modifier !== "undefined");
+                if(typeof step.maneuver.modifier !== "undefined"){
+                  tempstr += " on your " + step.maneuver.modifier.toUpperCase();
+                }
+                //this.route += tempstr;
+                console.log(tempstr);
+              }
+            }
           }
           else if(data.code === "NoRoute"){
             this.route = "No route was found."
